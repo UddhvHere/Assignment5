6e38927df2b3de6aff128021c2adc7091f0a05c3
UddhvHere <uddhavbhardwaj02@gmail.com>
2025-07-03 21:53:29 +0530

    Update Firsttask.py

--- Firsttask.py
+++ Firsttask.py
@@ -2,6 +2,6 @@ try:
     dict={'Alice':'85','Robin':'90','Harry':'67','Henry':'98'}
     name=input("Enter the student's name:")
     st="{}".format(name)
-    print(st,"'s marks:" ,dict[name].format(name))
+    print(f"{name}'s marks:" ,dict[name].format(name))
 except:
     print("Student Not Found")
